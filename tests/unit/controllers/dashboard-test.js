3d40100b4a54b024a8252518c3aaa8f73d895796
David Tang <dtang859@gmail.com>
2015-11-16 09:55:24 -0800

    update

--- tests/unit/controllers/dashboard-test.js
+++ tests/unit/controllers/dashboard-test.js
@@ -10,30 +10,28 @@ moduleFor('controller:dashboard', {
 test('it shows the latest cat added to my family', function(assert) {
   var controller = this.subject();
 
-  controller.set('model', Ember.ArrayProxy.create({
-    content: [
-      Ember.Object.create({
-        id: 1,
-        name: 'Tubby',
-        adopted_at: new Date('2015-09-25T21:44:24.6496202Z')
-      }),
-      Ember.Object.create({
-        id: 2,
-        name: 'Biscuit',
-        adopted_at: new Date('2015-10-21T23:30:54.217Z')
-      }),
-      Ember.Object.create({
-        id: 3,
-        name: 'Chester',
-        adopted_at: new Date('2015-09-29T21:37:33.1677559Z')
-      }),
-      Ember.Object.create({
-        id: 4,
-        name: 'Fiona',
-        adopted_at: new Date('2015-10-07T19:24:36.763Z')
-      })
-    ]
-  }));
+  controller.set('model', [
+    Ember.Object.create({
+      id: 1,
+      name: 'Tubby',
+      adopted_at: new Date('2015-09-25T21:44:24.6496202Z')
+    }),
+    Ember.Object.create({
+      id: 2,
+      name: 'Biscuit',
+      adopted_at: new Date('2015-10-21T23:30:54.217Z')
+    }),
+    Ember.Object.create({
+      id: 3,
+      name: 'Chester',
+      adopted_at: new Date('2015-09-29T21:37:33.1677559Z')
+    }),
+    Ember.Object.create({
+      id: 4,
+      name: 'Fiona',
+      adopted_at: new Date('2015-10-07T19:24:36.763Z')
+    })
+  ]);
 
   assert.equal(controller.get('mostRecentAdoptedCat.name'), 'Biscuit');
 });
